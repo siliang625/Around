9088bab06525fc45a7c55a9443a537f8a7b1661a
siliang625 <siliang625@users.noreply.github.com>
2018-05-15 21:10:56 -0700

    enable google cloud storage
    localhost:8080 has been taken, when deploy to cloud service, remember to
    change back to 8080

--- service/main.go
+++ service/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "context"
+    "cloud.google.com/go/storage"
     elastic "gopkg.in/olivere/elastic.v3"
     "fmt"
     "net/http"
@@ -9,6 +11,7 @@ import (
     "strconv"
     "reflect"
     "github.com/pborman/uuid"
+    "io"
 )
 
 type Location struct {
@@ -21,6 +24,7 @@ type Post struct {
     User     string `json:"user"`
     Message  string  `json:"message"`
     Location Location `json:"location"`
+    Url    string `json:"url"`
 }
 
 const (
@@ -32,6 +36,8 @@ const (
     //BT_INSTANCE = "around-post"
     // Needs to update this URL if you deploy it to cloud.
     ES_URL = "http://104.196.104.104:9200"
+    BUCKET_NAME = "post-image-204022"
+
 )
 
 
@@ -71,7 +77,7 @@ func main() {
     fmt.Println("started-service")
     http.HandleFunc("/post", handlerPost)
     http.HandleFunc("/search", handlerSearch)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -140,17 +146,107 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
     // Parse from body of request to get a json object.
-    fmt.Println("Received one post request")
-    decoder := json.NewDecoder(r.Body)
-    var p Post
-    if err := decoder.Decode(&p); err != nil {
-        panic(err)
-        return
+    // fmt.Println("Received one post request")
+    // decoder := json.NewDecoder(r.Body)
+    // var p Post
+    // if err := decoder.Decode(&p); err != nil {
+    //     panic(err)
+    //     return
+    // }
+    // id := uuid.New()
+    // // Save to ES.
+    // saveToES(&p, id)
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+    // 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+    // After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+    // If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+    r.ParseMultipartForm(32 << 20)
+
+    // Parse from form data.
+    fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+    lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+    lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+    p := &Post{
+           User:    "1111",
+           Message: r.FormValue("message"),
+           Location: Location{
+                  Lat: lat,
+                  Lon: lon,
+           },
     }
+
     id := uuid.New()
+
+    file, _, err := r.FormFile("image")
+    if err != nil {
+           http.Error(w, "Image is not available", http.StatusInternalServerError)
+           fmt.Printf("Image is not available %v.\n", err)
+           return
+    }
+    defer file.Close()
+    //https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-go
+    //use context to manipulate GCS credential
+    ctx := context.Background()
+
+   // replace it with your real bucket name.
+    _, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+    if err != nil {
+           http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+           fmt.Printf("GCS is not setup %v\n", err)
+           return
+    }
+
+    // Update the media link after saving to GCS.
+    p.Url = attrs.MediaLink
+
     // Save to ES.
-    saveToES(&p, id)
+    saveToES(p, id)
+
+    // Save to BigTable.
+    //saveToBigTable(p, id)
+
+
+
+
 }
+
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+      //create a client to manage GCS crednetial
+      client, err := storage.NewClient(ctx)
+      if err != nil {
+             return nil, nil, err
+      }
+      defer client.Close()
+
+      bucket := client.Bucket(bucketName)
+      // Next check if the bucket exists
+      if _, err = bucket.Attrs(ctx); err != nil {
+             return nil, nil, err
+      }
+      //now, bucket exists, grab the object
+      obj := bucket.Object(name)
+      w := obj.NewWriter(ctx)
+      if _, err := io.Copy(w, r); err != nil {
+             return nil, nil, err
+      }
+      if err := w.Close(); err != nil {
+             return nil, nil, err
+      }
+
+      //change access previledge: all people can read and read only
+      if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+             return nil, nil, err
+      }
+
+      attrs, err := obj.Attrs(ctx)
+      fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+      return obj, attrs, err
+}
+
+
 
 // Save a post to ElasticSearch
 func saveToES(p *Post, id string) {
@@ -176,64 +272,3 @@ func saveToES(p *Post, id string) {
 
     fmt.Printf("Post is saved to Index: %s\n", p.Message)
 }
-
-// To do: trim the string, otherwise a space would break the function
-// also the checking only peforms while searching
-//func containsFilteredWords(s *string) bool {
-//    filteredWords := []string{
-//        "fuck",
-//        "150",
-//    }
-//    for _, word := range filteredWords {
-//        if strings.Contains(*s, word) {
-//            return true
-//        }
-//    }
-//    return false
-//}
-
-
-//1. given a map from string to bool, how to iterate over it and print out all the key-values?
-//func main() {
-//    m := make(map[string]bool)
-//    m["jack"] = true
-//    m["john"] = false
-//    for k, v := range m {
-//        fmt.Println(k, v)
-//    }
-//}
-
-//2. given an array (slice) how to iterate all of its values?
-//func main()  {
-//    l := []string{"a", "b", "c"}
-//    for _, v := range l {
-//        fmt.Println(v)
-//    }
-//
-//}
-// _ indicate index
-
-//3. How to reduce the line of codes for this program
-//conn, err := os.Open("/tmp/file")
-//if err != nil {
-//panic(err)
-//} else {
-//conn.Read()
-//}
-//array := []string{} ?????
-//x := array[0]
-//y := array[1]
-//z := array[2]
-//fmt.Printf("%s %s %s\n", x, y, z)
-
-//if conn, err := os.Open("/tmp/file"); err != nil {
-//    panic(err)
-//}
-//conn.Read()
-//array := []string{}
-//x, y, z := array[0], array[1], array[2]
-//fmt.Printf("%s %s %s\n", x, y, z)
-
-// 4. how to append a value to a slice(array)
-//array = []int{}
-//append(array, 1)
